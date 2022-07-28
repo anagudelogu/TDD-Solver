4aaa4556ef887322ef3b1720156f8159343706b4
Andres Agudelo <anagudelogu@gmail.com>
2022-07-28 10:32:42 -0500

    Create test for factorial(0) case

--- spec/unit/solver_spec.rb
+++ spec/unit/solver_spec.rb
@@ -2,7 +2,10 @@ require_relative '../../lib/solver'
 
 RSpec.describe Solver do
   describe 'The factorial method' do
-    it 'Should return 1 if the input is 0'
+    it 'Should return 1 if the input is 0' do
+      solver = Solver.new
+      expect(solver.factorial(0)).to eq(1)
+    end
     it 'Should raise an error if the number is less than 0'
     it 'Should return the multiplication of all integers from 1 to the input'
   end
