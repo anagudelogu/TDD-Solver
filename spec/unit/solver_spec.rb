dd2760764a0d75b629dc06d1bcb1698e610341f9
Andres Agudelo <anagudelogu@gmail.com>
2022-07-28 10:39:05 -0500

    Create test for negative integer input on factorial method

--- spec/unit/solver_spec.rb
+++ spec/unit/solver_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe Solver do
       solver = Solver.new
       expect(solver.factorial(0)).to eq(1)
     end
-    it 'Should raise an error if the number is less than 0'
+    it 'Should raise an error if the number is less than 0' do
+      solver = Solver.new
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
     it 'Should return the multiplication of all integers from 1 to the input'
   end
 end
