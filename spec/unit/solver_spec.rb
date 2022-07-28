eb680e9d17eddffc660730ce6018addf341b6cda
Andres Agudelo <anagudelogu@gmail.com>
2022-07-28 11:01:09 -0500

    Refactor test suite to remove duplication and follow AAA pattern

--- spec/unit/solver_spec.rb
+++ spec/unit/solver_spec.rb
@@ -2,16 +2,19 @@ require_relative '../../lib/solver'
 
 RSpec.describe Solver do
   describe 'The factorial method' do
+    let(:solver) { Solver.new }
+
     it 'Should return 1 if the input is 0' do
-      solver = Solver.new
-      expect(solver.factorial(0)).to eq(1)
+      factorial_of0 = solver.factorial(0)
+
+      expect(factorial_of0).to eq(1)
     end
+
     it 'Should raise an error if the number is less than 0' do
-      solver = Solver.new
       expect { solver.factorial(-1) }.to raise_error(ArgumentError)
     end
+
     it 'Should return the multiplication of all integers from 1 to the input' do
-      solver = Solver.new
       factorial_of3 = solver.factorial(3)
       factorial_of6 = solver.factorial(6)
       factorial_of9 = solver.factorial(9)
