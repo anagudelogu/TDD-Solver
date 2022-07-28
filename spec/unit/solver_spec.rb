9157058457b8067e1504235e0768c62501dee85b
Andres Agudelo <anagudelogu@gmail.com>
2022-07-28 10:49:13 -0500

    Create test for factorial(n > 0) method

--- spec/unit/solver_spec.rb
+++ spec/unit/solver_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe Solver do
       solver = Solver.new
       expect { solver.factorial(-1) }.to raise_error(ArgumentError)
     end
-    it 'Should return the multiplication of all integers from 1 to the input'
+    it 'Should return the multiplication of all integers from 1 to the input' do
+      solver = Solver.new
+      factorial_of3 = solver.factorial(3)
+      factorial_of6 = solver.factorial(6)
+      factorial_of9 = solver.factorial(9)
+
+      expect(factorial_of3).to eq(6)
+      expect(factorial_of6).to eq(720)
+      expect(factorial_of9).to eq(362_880)
+    end
   end
 end
